e27e00ce2db9f66a8313bf08d87aefb0e2370a7b
Neto test <fin_parniska1997@mail.ru>
2023-07-16 12:24:56 +0500

    Корр. задание №1

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -9,11 +9,14 @@ holeGame.addEventListener('click', (event) => {
     } else {
         numberOfLost.innerText++;
     }
-    if(numberOfDeaths.innerText == '9') {
+    if(numberOfDeaths.innerText == 9) {
         alert ('Победа')
+        numberOfDeaths.innerText = 0;
+        numberOfLost.innerText = 0;
     }
     if(numberOfLost.innerText == 5) {
         alert ('Игра проиграна')
+        numberOfDeaths.innerText = 0;
+        numberOfLost.innerText = 0;
     }
 });
-
